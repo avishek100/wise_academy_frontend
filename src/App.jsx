f8f416e3921dd166b84d641add4b90c4d4d3eb86
avishek100 <avishek.chaudhary1002@gmail.com>
2025-02-22 00:00:40 +0545

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,91 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Footer from "./components/Footer.jsx";
-import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/auth";
+import RouteGuard from "./components/route-guard";
+import { useContext } from "react";
+import { AuthContext } from "./context/auth-context";
+import InstructorDashboardpage from "./pages/instructor";
+import StudentViewCommonLayout from "./components/student-view/common-layout";
+import StudentHomePage from "./pages/student/home";
+import NotFoundPage from "./pages/not-found";
+import AddNewCoursePage from "./pages/instructor/add-new-course";
+import StudentViewCoursesPage from "./pages/student/courses";
+import StudentViewCourseDetailsPage from "./pages/student/course-details";
+import PaypalPaymentReturnPage from "./pages/student/payment-return";
+import StudentCoursesPage from "./pages/student/student-courses";
+import StudentViewCourseProgressPage from "./pages/student/course-progress";
 
-import Courses from "./core/public/pages/Courses.jsx";
-import Hero from "./core/public/pages/Hero.jsx";
-import Home from "./core/public/pages/Home.jsx";
-import Login from "./core/public/pages/Login.jsx";
-import Signup from "./core/public/pages/Signup.jsx";
-import Dashboard from "./core/private/AdminDashboard.jsx";
+function App() {
+  const { auth } = useContext(AuthContext);
 
-const App = () => {
-    return (
-        <Router>
-            <div className="app-container">
-
-                <Navbar />
-                <div className="main-content">
-                    <Routes>
-                        <Route path="/" element={<Hero />} />
-                        <Route path="home" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/admindashboard" element={<Dashboard />} />
-                        <Route path="/addcourses" element={<Courses />} />
-
-
-                        {/*<Route path="/signup" element={<Signup />} />*/}
-
-                    </Routes>
-                </div>
-                <Footer />
-            </div>
-        </Router>
-    );
-};
+  return (
+    <Routes>
+      <Route
+        path="/auth"
+        element={
+          <RouteGuard
+            element={<AuthPage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/instructor"
+        element={
+          <RouteGuard
+            element={<InstructorDashboardpage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/instructor/create-new-course"
+        element={
+          <RouteGuard
+            element={<AddNewCoursePage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/instructor/edit-course/:courseId"
+        element={
+          <RouteGuard
+            element={<AddNewCoursePage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <RouteGuard
+            element={<StudentViewCommonLayout />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      >
+        <Route path="" element={<StudentHomePage />} />
+        <Route path="home" element={<StudentHomePage />} />
+        <Route path="courses" element={<StudentViewCoursesPage />} />
+        <Route
+          path="course/details/:id"
+          element={<StudentViewCourseDetailsPage />}
+        />
+        <Route path="payment-return" element={<PaypalPaymentReturnPage />} />
+        <Route path="student-courses" element={<StudentCoursesPage />} />
+        <Route
+          path="course-progress/:id"
+          element={<StudentViewCourseProgressPage />}
+        />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
 
 export default App;
