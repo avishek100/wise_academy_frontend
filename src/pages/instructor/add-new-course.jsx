6499587aaaca6134db5b5618f67ccc18fb5e2492
avishek100 <avishek.chaudhary1002@gmail.com>
2025-02-28 23:04:44 +0545

    add new course added

--- src/pages/instructor/add-new-course.jsx
+++ src/pages/instructor/add-new-course.jsx
@@ -82,9 +82,9 @@ function AddNewCoursePage() {
     const response =
       currentEditedCourseId !== null
         ? await updateCourseByIdService(
-            currentEditedCourseId,
-            courseFinalFormData
-          )
+          currentEditedCourseId,
+          courseFinalFormData
+        )
         : await addNewCourseService(courseFinalFormData);
 
     if (response?.success) {
@@ -170,3 +170,4 @@ function AddNewCoursePage() {
 }
 
 export default AddNewCoursePage;
+// 
