6f6eb0ad651ca4adada64b3b74af6fcf5d29eec7
avishek100 <avishek.chaudhary1002@gmail.com>
2025-02-28 23:03:45 +0545

    admin dashboard completed

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
     const [courses, setCourses] = useState([]);
@@ -21,3 +21,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+//
